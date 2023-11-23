0beba586a584ba21f030b2738303d11478ed9e34
carlos <carlosguemesdam@gmail.com>
2023-11-23 18:26:36 +0100

    PhoneLoginView cambios

--- lib/OnBoarding/PhoneLoginView.dart
+++ lib/OnBoarding/PhoneLoginView.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
-import '../KTPaddingText/PaddingClass.dart';
+import 'package:kyty/KTPaddingText/PaddingClass.dart';
 
 class PhoneLoginView extends StatefulWidget{
   @override
@@ -9,21 +8,21 @@ class PhoneLoginView extends StatefulWidget{
 }
 
 class _PhoneLoginViewState extends State<PhoneLoginView> {
-  TextEditingController tecPhone = TextEditingController();
-  TextEditingController tecVerify = TextEditingController();
-  String sVerificationCode ="";
-  bool blMostrarVerificacion = false;
+
+  TextEditingController tecPhone=TextEditingController();
+  TextEditingController tecVerify=TextEditingController();
+  String sVerificationCode="";
+  bool blMostrarVerificacion=false;
 
   void enviarTelefonoPressed() async{
-    String sTelefono = tecPhone.text;
+    String sTelefono=tecPhone.text;
 
     await FirebaseAuth.instance.verifyPhoneNumber(
-      phoneNumber: sTelefono,
-      verificationCompleted: verificacionCompletada,
-      verificationFailed: verificacionFallida,
-      codeSent: codeSent,
-      codeAutoRetrievalTimeout: codeAutoRetrievalTimeout,
-    );
+        phoneNumber: sTelefono,
+        verificationCompleted: verificacionCompletada,
+        verificationFailed: verificacionFallida,
+        codeSent: codigoEnviado,
+        codeAutoRetrievalTimeout: tiempoDeEsperaAcabado);
   }
 
   void enviarVerifyPressed() async{
@@ -36,28 +35,31 @@ class _PhoneLoginViewState extends State<PhoneLoginView> {
     // Sign the user in (or link) with the credential
     await FirebaseAuth.instance.signInWithCredential(credential);
 
-    Navigator.of(context).popAndPushNamed('/homeview');
+    Navigator.of(context).popAndPushNamed("/homeview");
   }
 
   void verificacionCompletada(PhoneAuthCredential credencial) async{
     await FirebaseAuth.instance.signInWithCredential(credencial);
-    Navigator.of(context).popAndPushNamed('/homeview');
+
+    Navigator.of(context).popAndPushNamed("/homeview");
   }
 
-  void verificacionFallida(FirebaseAuthException credencial){
-    if (credencial.code == 'invalid-phone-number') {
+  void verificacionFallida(FirebaseAuthException excepcion){
+    if (excepcion.code == 'invalid-phone-number') {
       print('The provided phone number is not valid.');
     }
   }
 
-  void codeSent(String codigo, int? iNumero) async{
-    sVerificationCode = codigo;
+  void codigoEnviado(String codigo, int? resendToken) async{
+    sVerificationCode=codigo;
     setState(() {
-      blMostrarVerificacion = true;
+      blMostrarVerificacion=true;
     });
+
+
   }
 
-  void codeAutoRetrievalTimeout(String sCadena){
+  void tiempoDeEsperaAcabado(String verID){
 
   }
 
@@ -67,14 +69,16 @@ class _PhoneLoginViewState extends State<PhoneLoginView> {
     return Scaffold(
       body: Column(
         children: [
-          PaddingClass(controlador: tecPhone, esContrasenya: false, labelText: "Número de teléfono"),
-          TextButton(onPressed: enviarTelefonoPressed, child: Text("Enviar Teléfono")),
-          if (blMostrarVerificacion)
-            PaddingClass(controlador: tecVerify, esContrasenya: false, labelText: "Número Verificación"),
-          if (blMostrarVerificacion)
-            TextButton(onPressed: enviarVerifyPressed, child: Text("Enviar Verificación")),
+          PaddingClass(labelText: "Numero Telefono",controlador: tecPhone, esContrasenya: false),
+          TextButton(onPressed: enviarTelefonoPressed, child: Text("Enviar")),
+          if(blMostrarVerificacion)
+            PaddingClass(labelText: "Numero Verificacion",controlador: tecVerify, esContrasenya: false),
+          if(blMostrarVerificacion)
+            TextButton(onPressed: enviarVerifyPressed, child: Text("Enviar"))
         ],
-      )
+
+      ),
+
     );
   }
 }
